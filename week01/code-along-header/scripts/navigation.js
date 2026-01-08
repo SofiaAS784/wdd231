14275adc2ade47a23d4afc2bbbee16d15ec85ea3
SofiaAS784 <ssuarez2@byupathway.edu>
2026-01-08 20:28:39 -0300

    w01 learning activities

--- week01/code-along-header/scripts/navigation.js
+++ week01/code-along-header/scripts/navigation.js
@@ -1,7 +1,15 @@
 // Store the selected elements that we are going to use.
 const navButton = document.querySelector('#ham-btn');
+const navigation = document.querySelector('#nav-bar');
 
 // Toggle the show class off and on
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
+    navigation.classList.toggle('show')
 });
+
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
