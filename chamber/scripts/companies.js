8976e238b2f54eafba8b0da99172c231d57c10f4
SofiaAS784 <ssuarez2@byupathway.edu>
2026-01-17 15:49:31 -0300

    w02 directory pt2

--- chamber/scripts/companies.js
+++ chamber/scripts/companies.js
@@ -1,26 +1,31 @@
-const data = 'data/members.json';
+const dataurl = 'https://sofiaas784.github.io/wdd231/chamber/data/members.json';
 
 const gridbtn = document.querySelector('#grid');
 const listbtn = document.querySelector('#list');
 const display = document.querySelector('article');
 
 async function getCompaniesData() {
-    const response = await fetch(data);
+    const response = await fetch(dataurl);
     const data = await response.json();
-    console.log(data.companies);
+    displayCompanies(data.companies);
 }
+
+getCompaniesData();
 
 gridbtn.addEventListener("click", () => {
     display.classList.add("grid");
     display.classList.remove("list");
+    getCompaniesData();
 });
 
 listbtn.addEventListener("click", () => {
     display.classList.add("list");
     display.classList.remove("grid");
+    getCompaniesData();
 });
 
 const displayCompanies = (companies) => {
+    display.innerHTML = "";
     companies.forEach((company) => {
         let address = document.createElement('p');
         let phone = document.createElement('p');
@@ -28,6 +33,8 @@ const displayCompanies = (companies) => {
         
         address.textContent = company.address;
         phone.textContent = company.phonenumber;
+        url.setAttribute('href', company.url);
+        url.setAttribute('target', "_blank");
         url.textContent = company.url;
         if (display.classList.contains('grid')) {
             let card = document.createElement('section');
@@ -36,8 +43,8 @@ const displayCompanies = (companies) => {
             logo.setAttribute('src', company.logo);
             logo.setAttribute('alt', `${company.name} logo`);
             logo.setAttribute('loading', 'lazy');
-            logo.setAttribute('width', '200');
-            logo.setAttribute('height', '75');
+            logo.setAttribute('width', '300');
+            logo.setAttribute('height', 'auto');
 
             card.appendChild(logo);
             card.appendChild(address);
