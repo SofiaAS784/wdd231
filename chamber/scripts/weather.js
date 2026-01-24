26cb843cab348f31395a95493ba9a63c5275fd21
SofiaAS784 <ssuarez2@byupathway.edu>
2026-01-24 15:08:22 -0300

    w03 update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,5 +1,4 @@
 const weatherCard = document.querySelector('#weather');
-const weatherIcon = document.querySelector('#weather-icon');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-38.01&lon=-57.55&units=metric&appid=955ff07b5d70345976e5288413f6aeb3';
 
@@ -22,23 +21,24 @@ apiFetch();
 
 function displayWeather(data) {
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', data.weather[0].description);
-
+    
+    let weatherIcon = document.createElement('img');
     let temp = document.createElement('h3');
     let desc = document.createElement('h3');
     let maxTemp = document.createElement('p');
     let minTemp = document.createElement('p');
     let humidity = document.createElement('p');
-    // let sunrise = document.createElement('p');
-    // let sunset = document.createElement('p');
-
+    
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+    weatherIcon.setAttribute('loading', 'lazy');
     temp.textContent = `${data.main.temp}°C`;
     desc.textContent = data.weather[0].description;
     maxTemp.textContent = `Max temp: ${data.main.temp_max}°`;
     minTemp.textContent = `Min temp: ${data.main.temp_min}°`;
     humidity.textContent = `Humidity: ${data.main.humidity}%`;
 
+    weatherCard.appendChild(weatherIcon);
     weatherCard.appendChild(temp);
     weatherCard.appendChild(desc);
     weatherCard.appendChild(maxTemp);
